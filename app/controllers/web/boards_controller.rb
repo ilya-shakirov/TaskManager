70f3062b0fc6c651aa5667a5276099bfbc46ffee
Ilya Shakirov <ilya.shakirov@dualbootpartners.com>
2022-11-23 15:20:57 +0300

    initial

--- app/controllers/web/boards_controller.rb
+++
@@ -1,6 +0,0 @@
-class Web::BoardsController < Web::ApplicationController
-  before_action :authenticate_user!
-  def show
-    render react_component: 'TaskBoard', props: {}
-  end
-end
--- test/factories/tasks.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :task do
-    name { "MyString" }
-    description { "MyText" }
-    author_id { 1 }
-    assignee_id { 1 }
-    expired_at { "2022-11-22" }
-  end
-end
